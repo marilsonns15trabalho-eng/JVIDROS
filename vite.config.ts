96587cb1ac96b45292cb0a921dbef2b3ffe92783
marilsonns15trabalho-eng <marilsonns15trabalho@gmail.com>
2026-08-13 18:56:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  root: "client",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

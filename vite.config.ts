f22d5870dda4e4bb51ade06b511be8eeb5b0a37b
marilsonns15trabalho-eng <marilsonns15trabalho@gmail.com>
2026-08-13 20:44:36 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
     envDir: path.resolve(import.meta.dirname),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
